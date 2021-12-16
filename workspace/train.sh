e7c6db3a6a1137c73eda9a93850784f7ecb052aa
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-16 16:17:05 +0900

    train

--- workspace/train.sh
+++ workspace/train.sh
@@ -1,3 +1,16 @@
 # Train YOLOv5s on COCO128 for 3 epochs
 # python train.py --img 640 --batch 16 --epochs 3 --data coco128.yaml --weights yolov5s.pt
-python $APP_PATH/train.py --img 640 --batch 16 --epochs 3 --data dataset.yaml --weights yolov5s.pt
+
+project=Tutorial
+model=yolov5s
+epochs=30
+name=damage
+
+python $APP_PATH/train.py \
+--img 640 \
+--batch 16 \
+--epochs $epochs \
+--data dataset.yaml \
+--weights yolov5s.pt \
+--project $project \
+--name $name 
