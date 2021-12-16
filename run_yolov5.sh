cd0c26aa76ab982c55f28ea883ba20ae8e160a15
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-16 16:16:39 +0900

    add env

--- run_yolov5.sh
+++ run_yolov5.sh
@@ -1,5 +1,11 @@
 workspace_local=$(pwd)/workspace
 workspace_remote='/usr/src/app/workspace'
 app_path='APP_PATH=/usr/src/app'
+workspace='workspace=/usr/src/app/workspace'
 
-docker run --rm --ipc=host --runtime=nvidia -it -p 8888:8888 --env=$app_path -v $workspace_local:$workspace_remote ultralytics/yolov5 bash
+docker run --rm --ipc=host --runtime=nvidia -it \
+-p 8888:8888 \
+--env=$app_path \
+--env=$workspace \
+-v $workspace_local:$workspace_remote \
+ultralytics/yolov5 bash
