5555b3211329580068ac261a84d5f683bd425e71
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-16 15:31:04 +0900

    run container

--- run_yolov5.sh
+++ run_yolov5.sh
@@ -1,4 +1,5 @@
 workspace_local=$(pwd)/workspace
 workspace_remote='/usr/src/app/workspace'
+app_path='APP_PATH=/usr/src/app'
 
-docker run --rm --ipc=host --runtime=nvidia -it -p 8888:8888 -v $workspace_local:$workspace_remote ultralytics/yolov5 bash
+docker run --rm --ipc=host --runtime=nvidia -it -p 8888:8888 --env=$app_path -v $workspace_local:$workspace_remote ultralytics/yolov5 bash
