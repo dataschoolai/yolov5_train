6c8f2e9c026b4beb0b28a7ec4707db5ad3c4f749
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-21 10:54:49 +0900

    project

--- workspace/train.sh
+++ workspace/train.sh
@@ -1,10 +1,10 @@
 # Train YOLOv5s on COCO128 for 3 epochs
 # python train.py --img 640 --batch 16 --epochs 3 --data coco128.yaml --weights yolov5s.pt
 
-project=Tutorial
+project=Efflorescence_train
 model=yolov5s
 epochs=30
-name=damage
+name=Efflorescence_225
 dataset=/usr/src/app/workspace/YOLOv5/dataset.yaml
 
 python $APP_PATH/train.py \
