b6bfe487a7bef91cb2c02f82c61db4d3de2fb104
labserver <uzbzarif@gmail.com>
2022-01-13 16:55:42 +0900

    start train

--- workspace/train.sh
+++ workspace/train.sh
@@ -1,6 +1,6 @@
 # Train YOLOv5s on COCO128 for 3 epochs
 # python train.py --img 640 --batch 16 --epochs 3 --data coco128.yaml --weights yolov5s.pt
-
+echo 'start'
 project=Efflorescence_train
 model=yolov5s
 epochs=30
